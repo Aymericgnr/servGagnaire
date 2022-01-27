fa3ea7d39547e431616c69f5bc1067daad3f02b5
aymericgnr <aymeric.gagnaire@gmail.com>
2022-01-27 09:53:12 +0100

    Création Controller

---
+++ src/Controller/ServGagnaireController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ServGagnaireController extends AbstractController
+{
+    /**
+     * @Route("/serv/gagnaire", name="serv_gagnaire")
+     */
+    public function index(): Response
+    {
+        return $this->render('serv_gagnaire/index.html.twig', [
+            'controller_name' => 'ServGagnaireController',
+        ]);
+    }
+}
