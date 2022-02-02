35f4ce3807ff9193376354b306a55d844528c4e2
aymericgnr <aymeric.gagnaire@gmail.com>
2022-02-02 15:51:09 +0100

    Vérification login et mdp formulaire

--- src/Controller/ServGagnaireController.php
+++ src/Controller/ServGagnaireController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServGagnaireController extends AbstractController
 {
@@ -18,12 +19,23 @@ class ServGagnaireController extends AbstractController
         ]);
     }
     /**
-     * @Route("/serv/login", name="login")
+     * @Route("/serv/login", name="serv_login")
      */
-    public function login(): Response
+    public function login(Request $request): Response
     {
+			//récupération des infos du formulaire.
+			$login = $request->request->get("Login");
+			$password = $request->request->get("Password");
+            $message = $request->request->get("message");
+            if ($login=="root" && $password=="toor")
+            $message = "login et mot de passe correct";
+            else
+                $message = "ATTENTION : login et mot de passe incorrect";       
         return $this->render('serv_gagnaire/login.html.twig', [
-            'controller_name' => 'ServGagnaireController',
+            'Login' => $login,
+            'Password' => $password,
+            'message'=> $message,
+        
         ]);
     }
 }
